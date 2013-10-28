466fae8b3cefac6f2ff58db95eebd98842261ea4
prolic <saschaprolic@googlemail.com>
2013-10-28 22:33:10 +0100

    add console usage and route config

--- config/module.config.php
+++ config/module.config.php
@@ -6,7 +6,27 @@ return array(
     ),
     'service_manager' => array(
         'factories' => array(
-            'HumusPHPUnitRunner' => 'HumusPHPUnitModule\Service\RunnerFactory'
+            'HumusPHPUnitRunner' => 'HumusPHPUnitModule\Service\RunnerFactory',
+        )
+    ),
+    'controllers' => array(
+        'factories' => array(
+            'HumusPHPUnitController' => 'HumusPHPUnitModule\Service\ControllerFactory'
+        ),
+    ),
+    'console' => array(
+        'router' => array(
+            'routes' => array(
+                'humusphpunit' => array(
+                    'options' => array(
+                        'route'    => 'humusphpunit [--strict] [--verbose|-v] [--debug] [--help|-h] [--version]',
+                        'defaults' => array(
+                            'controller' => 'HumusPHPUnitController',
+                            'action'     => 'run'
+                        )
+                    )
+                )
+            )
         )
     )
 );
