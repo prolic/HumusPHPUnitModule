c37af524f468466d52e7d4b56f195b2571846bfd
prolic <saschaprolic@googlemail.com>
2013-10-30 19:06:29 +0100

    cleanup in moduletest

--- tests/HumusPHPUnitModuleTest/ModuleTest.php
+++ tests/HumusPHPUnitModuleTest/ModuleTest.php
@@ -30,38 +30,4 @@ class ModuleTest extends \PHPUnit_Framework_TestCase
         $usage = $module->getConsoleUsage($console);
         $this->assertInternalType('array', $usage);
     }
-
-    /**
-     * @param Console $console
-     * @return array
-     */
-    public function getConsoleUsage(Console $console)
-    {
-        return array(
-            // Describe available commands
-            'humusphpunit [switches]'    => 'runs humusphpunit',
-
-            // Describe expected parameters
-            array(
-                '--strict',
-                'Run tests in strict mode.'
-            ),
-            array(
-                '--verbose|-v',
-                'Output more verbose information.'
-            ),
-            array(
-                '--debug',
-                'Display debugging information during test execution.'
-            ),
-            array(
-                '--help|-h',
-                'Prints thhe usage information.'
-            ),
-            array(
-                '--version',
-                'Prints the version and exits.'
-            ),
-        );
-    }
 }
